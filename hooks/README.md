e288f2ee0eab9366c86abc3fe24a06cc16bc1dc6
Azmi ŞAHİN <azmisahin@outlook.com>
2017-08-29 22:57:05 +0300

    Mobile Application With NodeJs / Cordova

---
+++ hooks/README.md
@@ -0,0 +1,14 @@
+<!--
+████████████████████████████████████████████████████████████████████████████████████████████████████
+* Name          :   Mobile Application
+* Version       :   @package.json.version
+* Description   :   Mobile Application With Cordova
+* Author        :   Azmi ŞAHİN <bilgi@azmisahin.com>
+* Licence       :   MIT
+════════════════════════════════════════════════════════════════════════════════════════════════════
+* Package       :   No Package / Web Application
+* Repository    :   https://github.com/azmisahin/com.domain.mobile.cordova.web.git
+* Homepage      :   https://azmisahin.github.io
+████████████████████████████████████████████████████████████████████████████████████████████████████
+-->
+# Cordova Hooks
---
+++ www/js/index.js
@@ -0,0 +1,22 @@
+/*
+████████████████████████████████████████████████████████████████████████████████████████████████████
+* Name          :   Mobile Application
+* Version       :   @package.json.version
+* Description   :   Mobile Application With Cordova
+* Author        :   Azmi ŞAHİN <bilgi@azmisahin.com>
+* Licence       :   MIT
+════════════════════════════════════════════════════════════════════════════════════════════════════
+* Package       :   No Package / Web Application
+* Repository    :   https://github.com/azmisahin/com.domain.mobile.cordova.web.git
+* Homepage      :   https://azmisahin.github.io
+████████████████████████████████████████████████████████████████████████████████████████████████████
+*/
+
+/// Application
+var app = {
+    // Application Constructor
+    initialize: function() {
+    }
+};
+
+app.initialize();
